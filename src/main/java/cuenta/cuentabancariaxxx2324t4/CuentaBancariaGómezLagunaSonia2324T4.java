96fe5754357b9e22243d5dcb3b401091576e9549
ASUS <ASUS@LAPTOP-JM51MH7G>
2024-03-04 18:19:49 +0100

    "Mi primer commit GómezLagunsSonia2324"

---
+++ src/main/java/cuenta/cuentabancariaxxx2324t4/CuentaBancariaGómezLagunaSonia2324T4.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package cuenta.cuentabancariaxxx2324t4;
+
+/**
+ *
+ * @author ASUS
+ */
+public class CuentaBancariaGómezLagunaSonia2324T4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/cuenta/cuentabancariaxxx2324t4/Main.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cuenta.cuentabancariaxxx2324t4;
+
+/**
+ *
+ * @author ASUS
+ */
+
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        CuentaGómezLagunaSonia2324T4 miCuentaGómezLagunaSonia2324T4;
+        double saldoActual;
+        int x;
+        
+        miCuentaGómezLagunaSonia2324T4 = retiraDineroGómezLagunaSonia();
+        
+        try
+        {
+            x=100;
+            System.out.println("Ingreso en cuenta de "+x+"€");
+            miCuentaGómezLagunaSonia2324T4.ingresar(x);
+        } catch (Exception e)
+        {
+            System.out.print("Fallo al ingresar");
+        }
+        saldoActual = miCuentaGómezLagunaSonia2324T4.estado();
+        System.out.println("El saldo actual es "+ saldoActual );
+    }
+
+    private static CuentaGómezLagunaSonia2324T4 retiraDineroGómezLagunaSonia() {
+        CuentaGómezLagunaSonia2324T4 miCuentaGómezLagunaSonia2324T4;
+        int x;
+        miCuentaGómezLagunaSonia2324T4 = new CuentaGómezLagunaSonia2324T4("Antonio Sánchez","ES55-3058-2365-8522-1234-5678",2500,0);
+        System.out.println("Saldo inicial de la cuenta: "+miCuentaGómezLagunaSonia2324T4.estado()+"€");
+        return ingresaDineroGómezLagunaSonia(miCuentaGómezLagunaSonia2324T4, "Ingreso en Cuenta");
+    }
+
+    private static CuentaGómezLagunaSonia2324T4 ingresaDineroGómezLagunaSonia(CuentaGómezLagunaSonia2324T4 miCuentaGómezLagunaSonia2324T4, String concepto) {
+        int x;
+        try
+        {
+            x=1500;
+            System.out.println("Retirada en cuenta de "+x+"€");
+            miCuentaGómezLagunaSonia2324T4.retirar(x);
+        } catch (Exception e)
+        {
+            System.out.print("Fallo al retirar");
+        }
+        return miCuentaGómezLagunaSonia2324T4;
+    }
+
+}
